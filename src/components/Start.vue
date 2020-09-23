3890a8c57c1d7572b797b2eff2c221ee3495c6cc
Jérémy DEMANGE <j@lachainemedias.com>
2020-09-23 04:41:39 +0200

    Ajout d'un fichier d'import

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -4,33 +4,98 @@
 			<v-row class="text-center">
 				<v-col class="mb-4">
 					<h1 class="display-2 font-weight-bold mb-3 mt-4">
-						Système de résolution de coréférence
+						Importer un texte et identifier les coréférences
 					</h1>
-
-					<p class="subheading font-weight-regular">
-						Entrez votre texte et téléchargez les résultats en un clic.
-					</p>
 				</v-col>
 
 				<v-col cols="12">
-					<v-textarea
-						v-model="texte"
-						label="Entrez votre texte ici..."
-						color="black"
-					></v-textarea>
-				</v-col>
-				<v-col class="mb-10" cols="12">
-					<v-btn
-						v-if="!loading"
-						block
-						color="black"
-						class="white--text"
-						@click="viewResult"
-						>Valider mon texte et télécharger les résultats</v-btn
-					>
-					<v-btn v-if="loading" block color="black" class="white--text" disabled
-						>Chargement en cours... (cela peut prendre plusieurs minutes)</v-btn
-					>
+					<v-stepper v-model="e1" class="myStepper">
+						<v-stepper-header>
+							<v-stepper-step color="warning" :complete="e1 > 1" step="1"
+								>Importation du texte</v-stepper-step
+							>
+
+							<v-divider></v-divider>
+
+							<v-stepper-step color="warning" :complete="e1 > 2" step="2"
+								>Paramètres</v-stepper-step
+							>
+
+							<v-divider></v-divider>
+
+							<v-stepper-step color="warning" step="3"
+								>Téléchargement des résultats</v-stepper-step
+							>
+						</v-stepper-header>
+
+						<v-stepper-items>
+							<v-stepper-content step="1">
+								<template>
+									<h3 class="mb-3">
+										Importer un fichier texte (en format classique ou en format
+										Iramuteq)
+									</h3>
+									<v-file-input
+										accept=".txt"
+										label="Importer un fichier texte"
+										color="black"
+										v-model="fileText"
+									></v-file-input>
+								</template>
+
+								<v-btn
+									block
+									color="warning"
+									@click="toImportText"
+									:disabled="!fileText"
+								>
+									Continuer
+								</v-btn>
+							</v-stepper-content>
+
+							<v-stepper-content step="2">
+								<h3 class="mb-3">
+									Vérifier et corriger votre texte
+								</h3>
+								<v-textarea
+									v-model="texte"
+									label="Entrez votre texte ici..."
+									color="black"
+								></v-textarea>
+								<v-btn
+									v-if="!loading"
+									block
+									color="warning"
+									class="white--text"
+									@click="viewResult"
+									>Valider mon texte et télécharger les résultats</v-btn
+								>
+								<v-btn
+									v-if="loading"
+									block
+									color="black"
+									class="white--text"
+									disabled
+									>Chargement en cours... (cela peut prendre plusieurs
+									minutes)</v-btn
+								>
+							</v-stepper-content>
+
+							<v-stepper-content step="3">
+								<v-card
+									class="mb-12"
+									color="grey lighten-1"
+									height="200px"
+								></v-card>
+
+								<v-btn color="primary" @click="e1 = 1">
+									Continue
+								</v-btn>
+
+								<v-btn text>Cancel</v-btn>
+							</v-stepper-content>
+						</v-stepper-items>
+					</v-stepper>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -52,9 +117,30 @@
 		data: () => ({
 			texte: "",
 			loading: false,
+			fileText: null,
+			e1: 1,
 		}),
 
 		methods: {
+			toImportText() {
+				// Test
+				const file = this.fileText;
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					this.texte = e.target.result;
+					// Traitements
+					let textTraitement = this.texte;
+					console.log();
+					textTraitement = textTraitement.replace(/\s[*]\S+/g, "");
+					textTraitement = textTraitement.replace("****", "");
+					this.texte = textTraitement;
+					// /Traitements
+				};
+				reader.readAsText(file);
+				// /Test
+
+				this.e1 += 1;
+			},
 			viewResult() {
 				let formData = new FormData();
 				let numberWatch = new Date().getUTCMilliseconds();
@@ -84,3 +170,9 @@
 		},
 	};
 </script>
+
+<style scoped>
+	.myStepper {
+		border: 3px solid black;
+	}
+</style>

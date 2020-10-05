5b5eb076c68ebe8b07362a08c2ffa4cd5252f9d3
Jérémy DEMANGE <j@lachainemedias.com>
2020-10-05 04:17:03 +0200

    Ajout du highlight

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import TextTruncate from "vue-text-truncate";
+import TextHighlight from "vue-text-highlight";
 
 Vue.config.productionTip = false;
 Vue.component("TextTruncate", TextTruncate);
+Vue.component("text-highlight", TextHighlight);
 
 new Vue({
 	vuetify,

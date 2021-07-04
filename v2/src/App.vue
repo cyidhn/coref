f136037d6f75625e3d01dd70f7acae36cbe87ca4
Jeremy Demange <jeremy@scrapfast.io>
2021-07-04 18:38:26 +0200

    Add folder

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Menu>
+    <MenuButton>More</MenuButton>
+    <MenuItems>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Account settings
+        </a>
+      </MenuItem>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Documentation
+        </a>
+      </MenuItem>
+      <MenuItem disabled>
+        <span class="opacity-75">Invite a friend (coming soon!)</span>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    components: {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

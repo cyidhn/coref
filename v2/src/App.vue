467fefe3414e28507c9ed2fd5c4cd4a0ce51bac1
Jeremy Demange <jeremy@scrapfast.io>
2021-07-04 18:43:14 +0200

    Add list of Algorithms

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,33 +1,16 @@
 <template>
-  <Menu>
-    <MenuButton>More</MenuButton>
-    <MenuItems>
-      <MenuItem v-slot="{ active }">
-        <a :class='{ "bg-blue-500": active }' href="/account-settings">
-          Account settings
-        </a>
-      </MenuItem>
-      <MenuItem v-slot="{ active }">
-        <a :class='{ "bg-blue-500": active }' href="/account-settings">
-          Documentation
-        </a>
-      </MenuItem>
-      <MenuItem disabled>
-        <span class="opacity-75">Invite a friend (coming soon!)</span>
-      </MenuItem>
-    </MenuItems>
-  </Menu>
+  <div>
+    <AlgorithmsList />
+  </div>
 </template>
 
 <script>
-  import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+  // import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+  import AlgorithmsList from "./components/AlgorithmsList.vue";
 
   export default {
     components: {
-      Menu,
-      MenuButton,
-      MenuItems,
-      MenuItem,
+      AlgorithmsList,
     },
   };
 </script>

c9c6a4fca6553c081bb50f06a2184807b28b761f
Jeremy Demange <jeremy@scrapfast.io>
2021-07-05 00:09:31 +0200

    Change elements

--- v2/src/components/AlgorithmsList.vue
+++ v2/src/components/AlgorithmsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-max fixed">
     <Listbox v-model="selectedPerson">
-      <div class="relative mt-1">
+      <div class="mt-1">
         <ListboxButton
           class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm"
         >
--- v2/src/components/Navbar.vue
+++ v2/src/components/Navbar.vue
@@ -1,11 +1,24 @@
 <template>
+<div>
     <div class="p-5">
         <div class="flex grid grid-cols-2">
-            <div><h1 class="text-4xl font-bold">Co.Ref</h1></div>
-            <div class="justify-self-end">aa</div>
+            <div><h1 class="text-4xl font-bold"><a href="/">Co.Ref</a></h1></div>
+            <div class="justify-self-end">
+                <AlgorithmsList />
+            </div>
         </div>
-        <AlgorithmsList />
+        <div class="mt-8">
+            <input type="text" class="rounded-md p-2 h-12 focus:outline-none border-black border-2 w-full" />
+        </div>
     </div>
+    <div class="mt-5 px-5 py-5 bg-black text-white text-lg text-center">
+        <a href="#" class="font-bold mx-3">Visualisation manuscrite</a> - 
+        <a href="#" class="mx-3">Visualisation colorique</a> - 
+        <a href="#" class="mx-3">Liste des mentions</a> - 
+        <a href="#" class="mx-3">Les paires</a> - 
+        <a href="#" class="mx-3">Les prédictions</a>
+    </div>
+</div>
 </template>
 
 <script>

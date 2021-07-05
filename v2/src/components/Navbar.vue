521f257ed581ee3b44eca5c5073836c3f4bae324
Jeremy Demange <jeremy@scrapfast.io>
2021-07-05 22:30:13 +0200

    Change navbar

--- v2/src/components/Navbar.vue
+++ v2/src/components/Navbar.vue
@@ -13,12 +13,27 @@
             <input type="text" v-model="search" class="rounded-md p-2 h-12 focus:outline-none border-black border-2 w-full" />
         </div>
     </div>
-    <div class="mt-5 px-5 py-5 bg-black text-white text-lg text-center">
-        <a @click="callViz('man')" class="cursor-pointer mx-3">Visualisation manuscrite</a> - 
+    <div class="mt-5 px-5 py-7 bg-black text-white text-lg text-center">
+        <span v-if="menuChange == 'man'">
+            <a @click="callViz('man')" class="cursor-pointer mx-3 pb-2 border-b-2 border-white">Visualisation manuscrite</a> - 
+        </span>
+        <span v-else>
+            <a @click="callViz('man')" class="cursor-pointer mx-3 pb-2">Visualisation manuscrite</a> - 
+        </span>
         <!-- <a @click="callViz('col')" class="cursor-pointer mx-3">Visualisation colorée</a> -  -->
-        <a @click="callViz('men')" class="cursor-pointer mx-3">Visualisation des mentions</a> - 
+        <span v-if="menuChange == 'men'">
+            <a @click="callViz('men')" class="cursor-pointer mx-3 pb-2 border-b-2 border-white">Visualisation des mentions</a> - 
+        </span>
+        <span v-else>
+             <a @click="callViz('men')" class="cursor-pointer mx-3 pb-2">Visualisation des mentions</a> - 
+        </span>
         <!-- <a @click="callViz('pai')" class="cursor-pointer mx-3">Les paires</a> -  -->
-        <a @click="callViz('pred')" class="cursor-pointer mx-3">Les paires et les prédictions</a>
+        <span v-if="menuChange == 'pred'">
+            <a @click="callViz('pred')" class="cursor-pointer mx-3 pb-2 border-b-2 border-white">Les paires et les prédictions</a>
+        </span>
+        <span v-else>
+            <a @click="callViz('pred')" class="cursor-pointer mx-3 pb-2">Les paires et les prédictions</a>
+        </span>
     </div>
 </div>
 </template>
@@ -36,6 +51,12 @@ export default {
         return { 
             search: "" 
         }
+    },
+
+    computed: {
+        menuChange() {
+            return this.$store.getters.menuChoice;
+        },
     },
 
     watch: {
